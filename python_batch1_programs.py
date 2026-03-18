fa08c1c296079d11805924fdc1d3bc3da2086817
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 10:35:17 +0800

    I created pseudocode for programs 1–10 to make coding easier.

---
+++ python_batch1_programs.py
@@ -0,0 +1,49 @@
+def prog01():
+# user needs to input num1 and num2
+# IF num1 > num2
+# DISPLAY num1
+# ELSE
+# DISPLAY num2 
+def prog02():
+    # INPUT num1, num2
+    # IF num1 == num2
+    #     DISPLAY "Equal"
+def prog03():
+    # INPUT num1, num2
+    # SET sum = num1 + num2
+def prog04():
+     # INPUT num1, num2
+    # SET product = num1 * num2
+    # DISPLAY product
+def prog05():
+    # INPUT num1, num2
+    # IF num2 != 0
+    #SET quotient = num1 / num2
+    # DISPLAY quotient
+    # ELSE
+    # DISPLAY "Cannot divide by zero"
+def prog06():
+     # INPUT base, exponent
+     # SET result = base ^ exponent
+     # DISPLAY result
+def prog07():
+    # SET sum = 0
+    # REPEAT 10 times
+    # INPUT number
+    # ADD number to sum
+    # DISPLAY sum
+def prog08():
+    # SET odd_count = 0
+    # REPEAT 10 times
+    # INPUT number
+    # IF number is odd
+    #     INCREMENT odd_coun
+    # DISPLAY odd_count
+def prog09():
+    # FOR number from 0 to 100
+    # IF number is even
+    #     DISPLAY number
+def prog10():
+    # FOR number from 0 to 100
+    # IF number mod 10 != 0
+    #     DISPLAY number
